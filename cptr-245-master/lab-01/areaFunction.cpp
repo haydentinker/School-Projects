8d17b66d781eecf73c0b151a9733db914b7f3f5d
Hayden Tinker <37011292+haydentinker@users.noreply.github.com>
2022-11-14 11:35:55 -0800

    Delete cptr-245-master directory

--- cptr-245-master/lab-01/areaFunction.cpp
+++
@@ -1,4 +0,0 @@
-#include "areaFunction.h"
-int findTheSquare(int side) { return side * side; }
-
-double areaOfACricle(int radius) { return 3.14159 * (radius * radius); }
--- cptr-245-master/lab-01/areaFunction.h
+++
@@ -1,2 +0,0 @@
-int findTheSquare(int side);
-double areaOfACricle(int radius);
